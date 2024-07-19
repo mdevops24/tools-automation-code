46b992edda6de3642854ad7fc365c3408e33c919
mohan <mohanreddy0532@gmail.com>
2024-07-19 12:17:57 +0530

    tools automation sg

--- infra-create/main.tf
+++ infra-create/main.tf
@@ -7,3 +7,26 @@ resource "aws_instance" "tool" {
     Name = var.name
   }
 }
+
+resource "aws_security_group" "tool-sg" {
+  name        = "${var.name}-sg"
+  description = "${var.name}-sg"
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port        = 22
+    to_port          = 22
+    protocol         = "TCP"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "${var.name}-sg"
+  }
+}
