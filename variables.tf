5b5d5c56943a7e013e14f329ccdb30525679b508
mohan <mohanreddy0532@gmail.com>
2024-07-26 15:08:12 +0530

    Tool infra vault infra

--- variables.tf
+++ variables.tf
@@ -6,12 +6,15 @@ variable "tools" {
           policy_name = [
             "AdministratorAccess"
            ]
+           ports = {}
       }
 
       vault = {
           instance_type = "t2.micro"
-          policy_name = [
-                 ]
+          policy_name = []
+          ports = {
+              vault = 8200
+              }
             }
     }
 }
